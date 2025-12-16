1adaaf6aa791bb9d5509ae201eb8fbf139a01906
Gihaluf <gihaluf.bojar@gmail.com>
2025-12-16 10:02:00 +0200

    Pabeigta

--- src/Loterija.java
+++ src/Loterija.java
@@ -1,38 +1,70 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JOptionPane;
-/*
- * Izmantojot JOptionPane klases dialoglodziņus datu ievadei un izvadei, sastādīt programmu, 
- * kura pielietojot steku realizēt loterijas trīs laimīgo skaitļu izlozi. Trīs piegājienos steks 
- * tiek aizpildīts ar 10 nejaušiem skaitļiem (bumbiņām) intervālā [0;9]. Skaitļi pa vienam tiek vilkti ārā no steka 
- * un viens no skaitļiem (steka virsējais skaitlis) nejauši tiek iegaumēts un ielikts laimējušo skaitļu stekā. 
- * Programma nodrošina izlozes izdarīšanu, piefiksējot izlozes veikšanas datumu un laiku, kā arī laimīgo skaitļu 
- * apskatīšanu. Pēc laimīgo skaitļu noteikšanas, izlozi atkārtoti var veikti tikai ja pagājusi vismaz 1 minūte
- */
+
 public class Loterija {
-	static void Aizpilda() {
-		Random rand = new Random();
-		
+	static Random rand = new Random();
+	static void Aizpilda(Stack<Integer> bumbas) {
+		bumbas.clear();
+		for(int i = 0; i < 10; i++) {
+			bumbas.push(rand.nextInt(10));
+		}
 	}
 	public static void main(String[] args) {
 		String izvele;
 		Stack<Integer> bumbas = new Stack<>();
 		Stack<Integer> laimigie = new Stack<>();
-		
-		String [] darbibas = {"Ievadīt vārdu", "Noteikt vārda garumu", "Apskatīt pēdējo burtu", "Apgriezt vārdu", "Apturēt"};
+		LocalDateTime pLaiks;
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		String FormatL = null;
+		String [] darbibas = {"Veikt izlozi", "Apskatīt laimīgos skaitļus","Apturēt"};
+		Long PLaiks = null;
 		
 		do {
 			izvele  = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Darbību saraksts", 
 					JOptionPane.INFORMATION_MESSAGE, null, darbibas, darbibas[0]);
 			if(izvele == null)
 				break;
-			switch(izvele) {
 			
-			case "Apskatīt pēdējo bumbu":
-				if(!bumbas.isEmpty()) {
+			switch(izvele) {
+			case "Veikt izlozi":
+				
+				if(!laimigie.isEmpty()) {
+					
+					Long ALaiks = System.currentTimeMillis();
+					Long starpiba = ALaiks - PLaiks;
+					System.out.println(starpiba);
+					if(starpiba  < 60000) {
+						JOptionPane.showMessageDialog(null, "Izlozi var veikt pēc 1 minūtes no pēdējās izlozes!", 
+								"Kļūda", JOptionPane.ERROR_MESSAGE);
+						break;
+					}else {
+						PLaiks = System.currentTimeMillis();
+						laimigie.clear();
+					}
+				}
+				if(laimigie.isEmpty()) {
+				PLaiks = System.currentTimeMillis();
+				for(int j=0; j<3; j++) {
+					pLaiks = LocalDateTime.now();
+					FormatL = pLaiks.format(myFormatObj);
+					Aizpilda(bumbas);
+					for(int i = 0; i < rand.nextInt(10); i++) {
+						bumbas.pop();
+					}
+					laimigie.push(bumbas.pop());
+					}
+				
+				}
+				break;
+			case "Apskatīt laimīgos skaitļus":
+				if(!laimigie.isEmpty()) {
 					JOptionPane.showMessageDialog(null, 
-							"Pēdējais burts ir: " + bumbas.peek(), 
+							"Izloze veikta: "+FormatL+
+							"\nLaimigie skaitļi: " + laimigie, 
 							"Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 				}else {
 					JOptionPane.showMessageDialog(null, "Steks ir tukšs!", 
